3bd6a0e2592641058f9ac7fb777082a61cfe859f
FranMedic <m.francisca.medico@gmail.com>
2021-10-06 17:22:51 +0200

    modified package.json and eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {},
+  rules: {
+    rules: {
+      "no-param-reassign": "off",
+      "no-plusplus": "off",
+      "no-restricted-syntax": "off",
+      "no-consistent-return": "off",
+      "no-debugger": "off",
+    },
+  },
 };
